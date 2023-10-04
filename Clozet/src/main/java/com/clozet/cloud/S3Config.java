99e1f54a89e4e087b9b29ff13ed6f9e13ec452f7
Sungyuun Kim <95204319+ungyuun@users.noreply.github.com>
2023-10-04 11:42:02 +0900

    (fix)  gitIgnore에 S3Config 추가

--- Clozet/src/main/java/com/clozet/cloud/S3Config.java
+++ Clozet/src/main/java/com/clozet/cloud/S3Config.java
@@ -20,18 +20,11 @@ public class S3Config {
 
     private String endPoint = "https://kr.object.ncloudstorage.com";
 
-//    @Value("${cloud.aws.region.static}")
-//    private String regionName;
-//
-//    @Value("${cloud.aws.credentials.access-key}")
-//    private String accessKey;
-//
-//    @Value("${cloud.aws.credentials.secret-key}")
-//    private String secretKey;
-    final String regionName = "kr-standard";
-    final String accessKey = "ezwltsG352nXDeTz7m7H";
-    final String secretKey = "PxZWLnZBdthl4W3bt79ItS1cb0JrKxCGorapBoMp";
-    private String bucketName = "clozet";
+
+    final String regionName = 
+    final String accessKey = 
+    final String secretKey = 
+    private String bucketName = 
     final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
             .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
             .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
